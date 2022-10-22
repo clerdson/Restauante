9d448fbe2446d8d7423f7f6ed5e47be25c6c6626
clerdson <clerdson@gmail.com>
2022-10-22 19:13:09 -0300

    Captulo 3(Displaying data from Rest Apis with Retrofit)

---
+++ app/src/main/java/com/example/restaurantsapp/RestaurantsApiService.kt
@@ -0,0 +1,9 @@
+package com.example.restaurantsapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface RestaurantsApiService {
+    @GET("restaurants.json")
+    fun getRestaurants():Call<List<Restaurant>>
+}
